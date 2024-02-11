602c6655014fd79c7a39bf39546570dc912e3d0f
Prafulla Kumar Sahu <prafulla.sahu@zee.com>
2024-02-11 09:14:12 +0530

    Add multiple consumer - do not create consumer like this, this is just example

--- src/main/java/com/serverwarrior/kafkaConsumer/consumer/KafkaMessageListener.java
+++ src/main/java/com/serverwarrior/kafkaConsumer/consumer/KafkaMessageListener.java
@@ -1,4 +1,9 @@
 package com.serverwarrior.kafkaConsumer.consumer;
 
 public interface KafkaMessageListener {
+  void consume(String message);
+  void consume2(String message);
+  void consume3(String message);
+  void consume4(String message);
+  void consume5(String message);
 }
--- src/main/java/com/serverwarrior/kafkaConsumer/consumer/impl/KafkaMessageListenerImpl.java
+++ src/main/java/com/serverwarrior/kafkaConsumer/consumer/impl/KafkaMessageListenerImpl.java
@@ -10,6 +10,23 @@ import org.springframework.stereotype.Service;
 public class KafkaMessageListenerImpl implements KafkaMessageListener {
   @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
   public void consume(String message) {
-    log.info("Consumed message: " + message);
+    log.info("Consumed message in 1st consumer: " + message);
+  }
+  //[Todo] do not create consumer like this, create proper concurrence consumers
+  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+  public void consume2(String message) {
+    log.info("Consumed message in 2nd consumer: " + message);
+  }
+  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+  public void consume3(String message) {
+    log.info("Consumed message in 3rd consumer: " + message);
+  }
+  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+  public void consume4(String message) {
+    log.info("Consumed message in 4th consumer: " + message);
+  }
+  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+  public void consume5(String message) {
+    log.info("Consumed message in 5th consumer: " + message);
   }
 }
