c91709c6a0030c35321f33fca486c40a26ab4367
Prafulla Kumar Sahu <prafulla.sahu@zee.com>
2024-02-12 09:28:57 +0530

    Kafka consumer config class and reading from particular partition

--- src/main/java/com/serverwarrior/kafkaConsumer/consumer/KafkaMessageListener.java
+++ src/main/java/com/serverwarrior/kafkaConsumer/consumer/KafkaMessageListener.java
@@ -1,9 +1,14 @@
 package com.serverwarrior.kafkaConsumer.consumer;
 
+import com.serverwarrior.kafkaConsumer.models.Customer;
+
 public interface KafkaMessageListener {
   void consume(String message);
-  void consume2(String message);
-  void consume3(String message);
-  void consume4(String message);
-  void consume5(String message);
+//  void consume2(String message);
+//  void consume3(String message);
+//  void consume4(String message);
+//  void consume5(String message);
+
+//  void consumeCustomer(Customer customer);
+  void consumerPartitionTopic(String message);
 }
--- src/main/java/com/serverwarrior/kafkaConsumer/consumer/impl/KafkaMessageListenerImpl.java
+++ src/main/java/com/serverwarrior/kafkaConsumer/consumer/impl/KafkaMessageListenerImpl.java
@@ -1,32 +1,52 @@
 package com.serverwarrior.kafkaConsumer.consumer.impl;
 
 import com.serverwarrior.kafkaConsumer.consumer.KafkaMessageListener;
+import com.serverwarrior.kafkaConsumer.models.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class KafkaMessageListenerImpl implements KafkaMessageListener {
+  @Override
   @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
   public void consume(String message) {
     log.info("Consumed message in 1st consumer: " + message);
   }
+
   //[Todo] do not create consumer like this, create proper concurrence consumers
-  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
-  public void consume2(String message) {
-    log.info("Consumed message in 2nd consumer: " + message);
-  }
-  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
-  public void consume3(String message) {
-    log.info("Consumed message in 3rd consumer: " + message);
-  }
-  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
-  public void consume4(String message) {
-    log.info("Consumed message in 4th consumer: " + message);
-  }
-  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
-  public void consume5(String message) {
-    log.info("Consumed message in 5th consumer: " + message);
+//  @Override
+//  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+//  public void consume2(String message) {
+//    log.info("Consumed message in 2nd consumer: " + message);
+//  }
+//  @Override
+//  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+//  public void consume3(String message) {
+//    log.info("Consumed message in 3rd consumer: " + message);
+//  }
+//  @Override
+//  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+//  public void consume4(String message) {
+//    log.info("Consumed message in 4th consumer: " + message);
+//  }
+//  @Override
+//  @KafkaListener(topics = "bulk-message-topic", groupId = "bulk-message-topic-listener-group")
+//  public void consume5(String message) {
+//    log.info("Consumed message in 5th consumer: " + message);
+//  }
+
+//  @Override
+//  @KafkaListener(topics = "customer-message-topic", groupId = "customer-message-topic-listener-group")
+//  public void consumeCustomer(Customer customer) {
+//    log.info("Consumed customer: " + customer.toString());
+//  }
+
+  @Override
+  @KafkaListener(topics = "message-to-partition", groupId = "message-to-partition-listener-group", topicPartitions = {@TopicPartition(topic="message-to-partition", partitions = {"2"})})
+  public void consumerPartitionTopic(String message) {
+    log.info("Consumed message from 3rd partition: " + message);
   }
 }
